52ebbceeae4658e9494f4416589b6cde66552730
AlexjrFS <afortunatosacramento@gmail.com>
2024-05-22 14:10:00 -0300

    first commit

---
+++ src/Contador.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Contador {
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+        System.out.println("Digite o primeiro valor");
+        int parametroUm = terminal.nextInt();
+        System.out.println("Digite o segundo valor");
+        int parametroDois = terminal.nextInt();
+        try{
+            contar(parametroUm, parametroDois);
+        } 
+        catch(ParametrosInvalidosException exception){
+            System.out.println(exception.getMessage());
+        }
+        terminal.close();
+    }
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException{
+        if(parametroUm>parametroDois){
+            throw new ParametrosInvalidosException("O segundo valor deve ser maior que o primeiro");
+        }
+        int contagem = parametroDois - parametroUm;
+        for(int i=1;i<=contagem;i++){
+            System.out.println("Imprimindo o número " + i);
+        }
+    }
+}
